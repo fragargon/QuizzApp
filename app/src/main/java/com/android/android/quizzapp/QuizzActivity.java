4793cfa65b62e479fddf2d0cb66be948488708fc
fragargon <fragargon@gmail.com>
2018-01-23 00:27:30 +0100

    add variable to find Views + instantiate Id's in onCreate method

--- app/src/main/java/com/android/android/quizzapp/QuizzActivity.java
+++ app/src/main/java/com/android/android/quizzapp/QuizzActivity.java
@@ -23,6 +23,15 @@ public class QuizzActivity extends AppCompatActivity {
     String playerName;
     int score;
 
+    // define variable to find the Views
+
+    CheckBox cbAnswer4A, cbAnswer4B, cbAnswer4C, cbAnswer4D, cbAnswer5A, cbAnswer5B, cbAnswer5C, cbAnswer5D, cbAnswer7A, cbAnswer7B, cbAnswer7C, cbAnswer7D;
+    RadioButton rbAnswer1C, rbAnswer2B, rbAnswer3B, rbAnswer8B, rbAnswer10B;
+    EditText etAnswer6, etAnswer9;
+    public TextView tv1, tv2;
+    Button myButton;
+    ScrollView sv;
+
     /**
      *
      * @param outState to restore previous state in orientation change
@@ -37,9 +46,7 @@ public class QuizzActivity extends AppCompatActivity {
     //Create a custom Method so tv1 and tv2 can be restore in configuration change
     public void customObj() {
         // Sent score and the player name when submit button is pressed in the layout up he screen
-        TextView tv1 = (TextView) findViewById(R.id.player_name);
         tv1.setText(playerName + " " + getString(R.string.yourScore) + " " + score);
-        TextView tv2 = (TextView) findViewById(R.id.hit_me);
         tv2.setText(getString(R.string.hit));
     }
 
@@ -56,18 +63,40 @@ public class QuizzActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
 
+        // Instantiate Ids to find the Views
+
+        sv = (ScrollView) findViewById(R.id.activity_quizz);
+        rbAnswer1C = (RadioButton) findViewById(R.id.radioButtonQ1C);
+        rbAnswer2B = (RadioButton) findViewById(R.id.radioButtonQ2B);
+        rbAnswer3B = (RadioButton) findViewById(R.id.radioButtonQ3B);
+        cbAnswer4A = (CheckBox) findViewById(R.id.checkBoxQ4A);
+        cbAnswer4B = (CheckBox) findViewById(R.id.checkBoxQ4B);
+        cbAnswer4C = (CheckBox) findViewById(R.id.checkBoxQ4C);
+        cbAnswer4D = (CheckBox) findViewById(R.id.checkBoxQ4D);
+        cbAnswer5A = (CheckBox) findViewById(R.id.checkBoxQ5A);
+        cbAnswer5B = (CheckBox) findViewById(R.id.checkBoxQ5B);
+        cbAnswer5C = (CheckBox) findViewById(R.id.checkBoxQ5C);
+        cbAnswer5D = (CheckBox) findViewById(R.id.checkBoxQ5D);
+        etAnswer6 = (EditText) findViewById(R.id.editTextQ6);
+        cbAnswer7A = (CheckBox) findViewById(R.id.checkBoxQ7A);
+        cbAnswer7B = (CheckBox) findViewById(R.id.checkBoxQ7B);
+        cbAnswer7C = (CheckBox) findViewById(R.id.checkBoxQ7C);
+        cbAnswer7D = (CheckBox) findViewById(R.id.checkBoxQ7D);
+        rbAnswer8B = (RadioButton) findViewById(R.id.radioButtonQ8B);
+        etAnswer9 = (EditText) findViewById(R.id.editTextQ9);
+        rbAnswer10B = (RadioButton) findViewById(R.id.radioButtonQ10B);
+        myButton = (Button) findViewById(R.id.buttonReview);
+        tv1 = (TextView) findViewById(R.id.player_name);
+        tv2 = (TextView) findViewById(R.id.hit_me);
+
         // Call the Intent that started this activity and extract the string
         Intent intent = getIntent();
         final String playerName = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         // Capture the layout's TextView and set the string as its text
-        TextView tv = (TextView) findViewById(R.id.player_name);
-        tv.setText(playerName + " " + getString(R.string.lets));
+        tv1.setText(playerName + " " + getString(R.string.lets));
 
-        // Find the View that shows the string hit_me
-        TextView playAgain = (TextView) findViewById(R.id.hit_me);
         // Set a click listener on that View
-        playAgain.setOnClickListener(new View.OnClickListener() {
-
+        tv2.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the text is clicked
             @Override
             public void onClick(View v) {
@@ -84,25 +113,21 @@ public class QuizzActivity extends AppCompatActivity {
             customObj();
         } else {
             score = 0;
-            tv.setText(playerName + " " + getString(R.string.lets));
+            tv1.setText(playerName + " " + getString(R.string.lets));
         }
 
-        // Find the View that shows the string hit_me
-        Button button = (Button) findViewById(R.id.buttonReview);
         // Set a click listener on that Button
-        button.setOnClickListener(new View.OnClickListener() {
+        myButton.setOnClickListener(new View.OnClickListener() {
 
             // The code in this method will be executed when the text is clicked
             @Override
             public void onClick(View view) {
 
                 // Scroll the view up the screen
-                ScrollView sv = (ScrollView) findViewById(R.id.activity_quizz);
                 sv.fullScroll(ScrollView.FOCUS_UP);
                 findViewById(R.id.player_name);
 
-                // Check the right RadioButton was selected question 1
-                RadioButton rbAnswer1C = (RadioButton) findViewById(R.id.radioButtonQ1C);
+                // Check the right RadioButton was selected Question 1
                 if (rbAnswer1C.isChecked()) {
                     score += 1;
                 } else {
@@ -111,8 +136,7 @@ public class QuizzActivity extends AppCompatActivity {
                     rbAnswer1C.setTextColor(rgb(213, 0, 0));
                 }
 
-                // Check the right RadioButton was selected question 2
-                RadioButton rbAnswer2B = (RadioButton) findViewById(R.id.radioButtonQ2B);
+                // Check the right RadioButton was selected Question 2
                 if (rbAnswer2B.isChecked()) {
                     score += 1;
                 } else {
@@ -121,8 +145,7 @@ public class QuizzActivity extends AppCompatActivity {
                     rbAnswer2B.setTextColor(rgb(213, 0, 0));
                 }
 
-                // Check the right RadioButton was selected question 3
-                RadioButton rbAnswer3B = (RadioButton) findViewById(R.id.radioButtonQ3B);
+                // Check the right RadioButton was selected Question 3
                 if (rbAnswer3B.isChecked()) {
                     score += 1;
                 } else {
@@ -131,11 +154,7 @@ public class QuizzActivity extends AppCompatActivity {
                     rbAnswer3B.setTextColor(rgb(213, 0, 0));
                 }
 
-                // Check the right CheckBox was selected question 4
-                CheckBox cbAnswer4A = (CheckBox) findViewById(R.id.checkBoxQ4A);
-                CheckBox cbAnswer4B = (CheckBox) findViewById(R.id.checkBoxQ4B);
-                CheckBox cbAnswer4C = (CheckBox) findViewById(R.id.checkBoxQ4C);
-                CheckBox cbAnswer4D = (CheckBox) findViewById(R.id.checkBoxQ4D);
+                // Check the right CheckBox was selected Question 4
                 if (cbAnswer4A.isChecked() && cbAnswer4B.isChecked() && cbAnswer4D.isChecked() && !cbAnswer4C.isChecked()) {
                     score += 1;
                 } else {
@@ -146,11 +165,7 @@ public class QuizzActivity extends AppCompatActivity {
                     cbAnswer4D.setTextColor(rgb(213, 0, 0));
                 }
 
-                // Check the right CheckBox was selected question 5
-                CheckBox cbAnswer5A = (CheckBox) findViewById(R.id.checkBoxQ5A);
-                CheckBox cbAnswer5B = (CheckBox) findViewById(R.id.checkBoxQ5B);
-                CheckBox cbAnswer5C = (CheckBox) findViewById(R.id.checkBoxQ5C);
-                CheckBox cbAnswer5D = (CheckBox) findViewById(R.id.checkBoxQ5D);
+                // Check the right CheckBox was selected Question 5
                 if (cbAnswer5B.isChecked() && cbAnswer5C.isChecked() && cbAnswer5D.isChecked() && !cbAnswer5A.isChecked()) {
                     score += 1;
                 } else {
@@ -161,8 +176,7 @@ public class QuizzActivity extends AppCompatActivity {
                     cbAnswer5D.setTextColor(rgb(213, 0, 0));
                 }
 
-                // Check the right entered answer is given question 6
-                EditText etAnswer6 = (EditText) findViewById(R.id.editTextQ6);
+                // Check the right entered answer is given Question 6
                 if (etAnswer6.getText().toString().equals(getString(R.string.Q6A))) {
                     score += 1;
                 } else {
@@ -172,11 +186,7 @@ public class QuizzActivity extends AppCompatActivity {
                     etAnswer6.setTextColor(rgb(213, 0, 0));
                 }
 
-                // Check the right CheckBox was selected question 7
-                CheckBox cbAnswer7A = (CheckBox) findViewById(R.id.checkBoxQ7A);
-                CheckBox cbAnswer7B = (CheckBox) findViewById(R.id.checkBoxQ7B);
-                CheckBox cbAnswer7C = (CheckBox) findViewById(R.id.checkBoxQ7C);
-                CheckBox cbAnswer7D = (CheckBox) findViewById(R.id.checkBoxQ7D);
+                // Check the right CheckBox was selected Question 7
                 if (cbAnswer7B.isChecked() && cbAnswer7C.isChecked() && cbAnswer7D.isChecked() && !cbAnswer7A.isChecked()) {
                     score += 1;
                 } else {
@@ -187,8 +197,7 @@ public class QuizzActivity extends AppCompatActivity {
                     cbAnswer7D.setTextColor(rgb(213, 0, 0));
                 }
 
-                // Check the right RadioButton was selected question 8
-                RadioButton rbAnswer8B = (RadioButton) findViewById(R.id.radioButtonQ8B);
+                // Check the right RadioButton was selected Question 8
                 if (rbAnswer8B.isChecked()) {
                     score += 1;
                 } else {
@@ -197,8 +206,7 @@ public class QuizzActivity extends AppCompatActivity {
                     rbAnswer8B.setTextColor(rgb(213, 0, 0));
                 }
 
-                // Check the right entered answer is given question 9
-                EditText etAnswer9 = (EditText) findViewById(R.id.editTextQ9);
+                // Check the right entered answer is given Question 9
                 if (etAnswer9.getText().toString().equals(getString(R.string.Q9B))) {
                     score += 1;
                 } else {
@@ -208,8 +216,7 @@ public class QuizzActivity extends AppCompatActivity {
                     etAnswer9.setTextColor(rgb(213, 0, 0));
                 }
 
-                // Check the right RadioButton was selected question 10
-                RadioButton rbAnswer10B = (RadioButton) findViewById(R.id.radioButtonQ10B);
+                // Check the right RadioButton was selected Question 10
                 if (rbAnswer10B.isChecked()) {
                     score += 1;
                 } else {
@@ -219,13 +226,11 @@ public class QuizzActivity extends AppCompatActivity {
                 }
 
                 //Prevents the Submit method from being pressed more than once
-                Button myButton = (Button) findViewById(R.id.buttonReview);
                 myButton.setClickable(false);
 
                 // Sent score and the player name if submit button is pressed in the layout up he screen
-                TextView tv1 = (TextView) findViewById(R.id.player_name);
+
                 tv1.setText(playerName + " " + getString(R.string.yourScore) + " " + score);
-                TextView tv2 = (TextView) findViewById(R.id.hit_me);
                 tv2.setText(getString(R.string.hit));
 
                 //Display Toast message with the score
